14f8e5c67f32a0b40f707b4df142b0e0d87490b0
releventhal <renatahl@al.insper.edu.br>
2026-08-13 09:22:27 -0300

    final handout 1

---
+++ handout1/utils.py
@@ -0,0 +1,44 @@
+import json
+from pathlib import Path
+
+def extract_route(request):
+    primeira_linha = request.split('\n')[0]
+    rota = primeira_linha.split(' ')[1]
+    return rota[1:]
+
+def read_file(path):
+    with open(path, "rb") as file:
+        return file.read()
+
+def load_data(filename):
+    path = Path("data") / filename
+
+    with open(path, "r") as file:
+        return json.load(file)
+
+def load_template(filename):
+    path = Path("templates") / filename
+
+    with open(path, "r") as file:
+        return file.read()
+
+
+def add_note(note):
+    notes = load_data('notes.json')
+    notes.append(note)
+
+    path = Path('data') / 'notes.json'
+
+    with open(path, 'w') as file:
+        json.dump(notes, file)
+
+def build_response(body='', code=200, reason='OK', headers=''):
+    response = f'HTTP/1.1 {code} {reason}\n'
+
+    if headers:
+        response += headers + '\n'
+
+    response += '\n'
+    response += body
+
+    return response.encode()
---
+++ handout1/views.py
@@ -0,0 +1,42 @@
+from utils import load_data, load_template, add_note, build_response
+from urllib.parse import unquote_plus
+
+def index(request):
+
+    # A string de request sempre começa com o tipo da requisição (ex: GET, POST)
+    if request.startswith('POST'):
+        request = request.replace('\r', '')  # Remove caracteres indesejados
+        # Cabeçalho e corpo estão sempre separados por duas quebras de linha
+        partes = request.split('\n\n')
+        corpo = partes[1]
+        params = {}
+        # Preencha o dicionário params com as informações do corpo da requisição
+        # O dicionário conterá dois valores, o título e a descrição.
+        # Posteriormente pode ser interessante criar uma função que recebe a
+        # requisição e devolve os parâmetros para desacoplar esta lógica.
+        # Dica: use o método split da string e a função unquote_plus
+        for chave_valor in corpo.split('&'):
+            chave, valor = chave_valor.split('=')
+            params[chave] = unquote_plus(valor)
+
+        add_note(params)
+
+        return build_response(
+        code=303,
+        reason='See Other',
+        headers='Location: /'
+        )
+
+
+
+    # Cria uma lista de <li>'s para cada anotação
+    # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
+    note_template = load_template('components/note.html')
+    notes_li = [
+        note_template.format(title=dados['titulo'], details=dados['detalhes'])
+        for dados in load_data('notes.json')
+    ]
+    notes = '\n'.join(notes_li)
+
+    body = load_template('index.html').format(notes=notes)
+    return build_response(body)
